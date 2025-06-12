0138ba2dcd03c2124287b6d4b55b5377210b5c3f
jm-2oo <jennyy.man@hotmail.com>
2025-06-12 10:00:43 +0100

    moved code to gitignore

--- misc_code/write_to_csv.py
+++
@@ -1,8 +0,0 @@
-import csv 
-
-with open('reed.csv', 'w', newline='') as file:
-    writer = csv.DictWriter(file, fieldnames=job_list[0].keys())
-    writer.writeheader()
-    writer.writerows(job_list)
-
-print("CSV saved: reed.csv")
